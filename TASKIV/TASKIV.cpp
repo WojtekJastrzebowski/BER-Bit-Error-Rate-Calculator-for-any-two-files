c1c61669835b44799b56369b61e289bff39887a6
Wojtek Jastrzebowski <jastrzebowskiweb@gmail.com>
2022-06-24 19:58:02 +0200

    dodanie funkcji hammingDistance

--- TASKIV/TASKIV.cpp
+++ TASKIV/TASKIV.cpp
@@ -90,3 +90,15 @@ int main(int argc, char** argv)
     closeLog();
     return 0;
 }
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2; // XOR
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
+}
